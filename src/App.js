c25069f9beaa70a942988f496a53b4d1d926aa34
subhradwi01 <subhradwi01@gmail.com>
2023-03-10 01:20:46 +0530

    3d website done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ScrollControls, Scroll, SpotLight, Environment, Sparkles, Backdrop, Float, Ring } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { Robot } from "./Component/Robot";
+import baffle  from "baffle";
 
 function App() {
+  
+  useEffect(()=>{
+    const heading = baffle(".title");
+    heading.set({
+      characters :"░P░H░A░N░T░O░M",
+      speed:100
+    })
+    heading.start();
+    heading.reveal(1000,1000)
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <color attach="background" args={["#333333"]} />
+      <ambientLight intensity={0.1} />
+      <SpotLight
+        position={[0, 3, 0]}
+        angle={1.3}
+        penumbra={1}
+        castShadow
+        intensity={2}
+        shadow-bias={-0.0001}
+      />
+      <Environment
+        // background
+        preset="warehouse"
+       />
+      <ScrollControls pages={6} damping={0.1}>
+          <Sparkles size={2} scale={[10,10,10]} color={'#fff'}/>
+          <Robot scale={0.9}/>
+          <Backdrop
+          receiveShadow
+          floor={20.5} // Stretches the floor segment, 0.25 by default
+          segments={100} // Mesh-resolution, 20 by default
+          scale={[50,30,10]}
+          position={[4,-10,0]}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <meshStandardMaterial color="#0a1a1f" />
+        </Backdrop> 
+        <Float
+          speed={4} // Animation speed, defaults to 1
+          rotationIntensity={0.5} // XYZ rotation intensity, defaults to 1
+          floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+          floatingRange={[1, 1]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+        >
+        <Ring scale={3.5} position-z={-2.5} position-y={-1} args={[0, 0.95, 60]} receiveShadow>
+          <meshStandardMaterial color="#2a3a3f" />
+        </Ring>
+        </Float>
+        <Scroll>
+        </Scroll>
+        <Scroll html style={{ width: "100%" }}>
+          <h1
+            className="title"
+            style={{
+              color: "#cdcbca",
+              position: "absolute",
+              top: `65vh`,
+              left: "50%",
+              fontSize: "13em",
+              transform: `translate(-50%,-50%)`,
+            }}
+          >
+            PHANTOM
+          </h1>
+
+          <div className="row" style={{ position: "absolute", top: `132vh` }}>
+            <h2>Be a Man of the Future.</h2>
+            <p style={{ maxWidth: "400px" }}>
+              Featuring a sleek, metallic design inspired by advanced
+              technology, this aftershave bottle is as stylish as it is
+              functional. But it's not just a pretty face - inside, you'll find
+              a nourishing and protective aftershave formula that will leave
+              your skin feeling refreshed and hydrated.
+            </p>
+            <button>Read more</button>
+          </div>
+
+          <div className="row" style={{ position: "absolute", top: `230vh` }}>
+            <div
+              className="col"
+              style={{ position: "absolute", right: `40px`, width: "540px" }}
+            >
+              <h2 style={{ maxWidth: "440px" }}>Tech-Savvy Side</h2>
+              <p style={{ maxWidth: "440px" }}>
+                Featuring a sleek, metallic design inspired by advanced
+                technology, this aftershave bottle is as stylish as it is
+                functional. But it's not just a pretty face - inside, you'll
+                find a nourishing and protective aftershave formula that will
+                leave your skin feeling refreshed and hydrated.
+              </p>
+              <button>Read more</button>
+            </div>
+          </div>
+
+          <h2
+            style={{
+              position: "absolute",
+              top: "350vh",
+              left: "50%",
+              transform: `translate(-50%,-50%)`,
+            }}
+          >
+            Cutting-Edge of Grooming
+          </h2>
+
+          <button
+            style={{
+              position: "absolute",
+              top: `590vh`,
+              left: "50%",
+              transform: `translate(-50%,-50%)`,
+            }}
+          >
+            Buy now
+          </button>
+        </Scroll>
+      </ScrollControls>
+    </>
   );
 }
 
